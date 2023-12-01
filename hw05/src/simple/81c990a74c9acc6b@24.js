062bb66116fa614753f5996418d634d87a666d06
nicky <lkjy3697n@gmail.com>
2023-12-01 15:42:52 +0800

    Fix bug in login functionality

--- hw05/src/simple/81c990a74c9acc6b@24.js
+++ hw05/src/simple/81c990a74c9acc6b@24.js
@@ -131,7 +131,7 @@ export default function define(runtime, observer) {
   const main = runtime.module();
   function toString() { return this.url; }
   const fileAttachments = new Map([
-    ["output@1.json", {url: new URL("../output@1.json", import.meta.url), mimeType: "application/json", toString}]
+    ["output@1.json", {url: new URL("../output1.json", import.meta.url), mimeType: "application/json", toString}]
   ]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], _1);
